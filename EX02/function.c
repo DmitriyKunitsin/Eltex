f5945bb244ca6302ce605177c465461f11899d89
DmitriyKunitsin <DmitriyKunitsin>
2024-06-27 13:57:00 +0700

    add make and done EX_1.c

---
+++ EX02/function.c
@@ -0,0 +1,25 @@
+#include "function.h"
+
+int changeThirdByte(int value) {
+  int inputUserValue = input();
+  char* ptr;
+  ptr = (char*)&value;
+  ptr += 3;
+  *ptr = inputUserValue;
+  printf("\n%d\n", value);
+  return value;
+}
+
+int input() {
+  int val = 0;
+  __uint8_t checkValidation = 0;
+  do {
+    printf("Введите целое положительное число: ");
+    if ((checkValidation = scanf("%d", &val)) != 1 || val < 0) {
+      printf("Некорректный ввод. Пожалуйста, введите положительное число.\n");
+      while (getchar() != '\n') {
+      };  // Очистка буфера ввода
+    }
+  } while (checkValidation != 1 || val < 0);
+  return val;
+}
---
+++ EX02/function.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <stdio.h>
+
+
+int input();
+int changeThirdByte(int value);
+
+#endif

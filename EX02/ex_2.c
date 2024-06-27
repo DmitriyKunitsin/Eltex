0419055adacf1360a30ddbe45691e5ac4c37aa90
DmitriyKunitsin <DmitriyKunitsin>
2024-06-27 14:01:58 +0700

    add ex2 and change make

--- EX02/ex_2.c
+++ EX02/ex_2.c
@@ -13,4 +13,16 @@ float *xp = &x; // TODO: отредактируйте эту строку, и т
 уравнения
 float *yp = &y;
 printf("Результат: %f\n", *xp + *yp);
-}*/
+}
+*/
+#include <stdio.h>
+int main(void) {
+  float x = 5.0;
+  printf("x = %f, ", x);
+  float y = 6.0;
+  printf("y = %f\n", y);
+  float *xp = &x + 1;
+
+  float *yp = &y;
+  printf("Результат: %.1f\n", *xp + *yp); // поставил еще ограничение по точности, ТЗ требует вывести один 0 после точки
+}
